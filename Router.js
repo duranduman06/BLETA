76b779e1273eca7b063aa4b589642ab104d3a547
umut güler <umutguler@umut-MacBook-Pro.local>
2024-07-27 18:51:11 +0300

    add router.js

--- Router.js
+++ Router.js
@@ -5,16 +5,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomePage from './src/screens/homePage/HomePage';
 import MyAccount from './src/screens/myAccount/MyAccount';
 import Login from './src/screens/login/Login';
+import HouseCleaning from './src/screens/details/houseCleaning/HouseCleaning';
 
 const Stack = createStackNavigator();
 
 const RouterComp = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator name='mainStack' initialRouteName='myAccount'>
+      <Stack.Navigator name='mainStack' initialRouteName='houseCleaning'>
         <Stack.Screen name='homePage' options={{ headerShown: false }} component={HomePage} />
         <Stack.Screen name='myAccount' options={{ headerShown: false }} component={MyAccount} />
         <Stack.Screen name='login' options={{ headerShown: false }} component={Login} />
+        <Stack.Screen name='houseCleaning' options={{ headerShown: false }} component={HouseCleaning} />
+
       </Stack.Navigator>
     </NavigationContainer>
   )
